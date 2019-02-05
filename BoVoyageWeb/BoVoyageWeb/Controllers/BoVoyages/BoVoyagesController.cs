1be6a9289ef1f343f9c60c819471aaca975ed387
Marc <marc.dreyfus.ecalion@gmail.com>
2019-02-05 15:34:40 +0100

    second commit

---
+++ BoVoyageWeb/BoVoyageWeb/Controllers/BoVoyages/BoVoyagesController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BoVoyageWeb.Controllers.BoVoyages
+{
+    public class BoVoyagesController : Controller
+    {
+        // GET: BoVoyage
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ListVoyages()
+        {
+            return View();
+        }
+    }
+}
